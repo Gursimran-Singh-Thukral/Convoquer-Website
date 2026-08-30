6b133891c1457fb0c7ab0e05416e80dc6532266e
cls <gursimransinghthukral@gmail.com>
2026-08-30 19:03:37 +0530

    chore: configure prisma database client

---
+++ server/prisma/migrations/20260830100024_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "PrismaTest" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "PrismaTest_pkey" PRIMARY KEY ("id")
+);
