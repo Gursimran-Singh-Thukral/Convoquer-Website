9bbed4ab3e91ed7319e49c62d1bd7ed0ada510bf
cls <gursimransinghthukral@gmail.com>
2026-08-30 19:25:57 +0530

    test: establish testing infrastructure

---
+++ client/src/test/example.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from "vitest";
+
+describe('Convoquer Client', () => {
+
+    it('runs the test environment', () => {
+
+        expect(true).toBe(true);
+
+    })
+
+})
